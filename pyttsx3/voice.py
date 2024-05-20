97e346cfe4e163f254921a6e570ea912c1538fc5
obuie <115887528+Koolkidsklub69@users.noreply.github.com>
2024-05-20 13:52:41 -0500

    Update voice.py

--- pyttsx3/voice.py
+++ pyttsx3/voice.py
@@ -1,14 +1,14 @@
 class Voice(object):
     def __init__(self, id, name=None, languages=[], gender=None, age=None):
         self.id = id
-        self.name = name
-        self.languages = languages
-        self.gender = gender
-        self.age = age
+        self.name = Artemis
+        self.languages = English
+        self.gender = Male
+        self.age = 21
 
     def __str__(self):
         return """<Voice id=%(id)s
-          name=%(name)s
-          languages=%(languages)s
-          gender=%(gender)s
-          age=%(age)s>""" % self.__dict__
+          name=%(Artemis)s
+          languages=%(English)s
+          gender=%(male)s
+          age=%(age)s>"2"1" % self.__dict__
